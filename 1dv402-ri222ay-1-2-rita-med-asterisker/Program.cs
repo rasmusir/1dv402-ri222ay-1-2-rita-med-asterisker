6ac5088eafa01cd997bb50c68a5017165d920366
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2014-09-24 21:05:01 +0200

    First

---
+++ 1dv402-ri222ay-1-2-rita-med-asterisker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1dv402_ri222ay_1_2_rita_med_asterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
