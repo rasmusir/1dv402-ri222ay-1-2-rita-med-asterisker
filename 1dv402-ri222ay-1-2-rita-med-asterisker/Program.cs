606d7d38f87c3c2bd314acc8cc6a1310e195f985
Rasmus Israelsson <israelssonrasmusraz@gmail.com>
2014-09-24 22:58:16 +0200

    +RenderDiamond(byte maxCount) : void +RenderRow(int maxCount, int asteriskCount) : void +ReadOddByte(string prompt = null, byte maxValue = 255) : byte +IsContinuing() : void +ShowMessage(string message, bool error = false) : void

--- 1dv402-ri222ay-1-2-rita-med-asterisker/Program.cs
+++ 1dv402-ri222ay-1-2-rita-med-asterisker/Program.cs
@@ -10,6 +10,67 @@ namespace _1dv402_ri222ay_1_2_rita_med_asterisker
     {
         static void Main(string[] args)
         {
+            byte b = ReadOddByte("Skriv in ett tal: ", 71);
+            RenderDiamond(b);
+        }
+
+        static void RenderDiamond(byte maxCount)
+        {
+            for (int i = 0; i < maxCount; i++ )
+            {
+                int count = 0;
+                if (i <= maxCount / 2)
+                    count =  i * 2 + 1;
+                else
+                    count = (maxCount - i) * 2 - 1;
+                RenderRow(maxCount, count);
+            }
+        }
+
+        static void RenderRow(int maxCount, int asteriskCount)
+        {
+            int start = maxCount / 2 - asteriskCount/2;
+            Console.CursorLeft = start;
+            for (int i = 0; i < asteriskCount; i++ )
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        static byte ReadOddByte(string prompt = null, byte maxValue = 255)
+        {
+            while(true)
+            {
+                if (prompt != null)
+                    Console.Write(prompt);
+                byte b = maxValue;
+                if (byte.TryParse(Console.ReadLine(),out b))
+                {
+                    if (b >= 1 && b <= maxValue && b%2 == 1)
+                        return b;
+                }
+                ShowMessage(string.Format("FEL! Det inmatade värdet är inte ett udda heltal mellan 1 och {0}", maxValue),true);
+            }
+        }
+
+        static bool IsContinuing()
+        {
+            ShowMessage("Tryck tangent för att fortsätta - Esc avsluta.");    // TODO Replace with resource later.
+            if (Console.ReadKey().Key == ConsoleKey.Escape)
+                return false;
+            return true;
+        }
+
+        static void ShowMessage(string message,bool error = false)
+        {
+            if (error)
+                Console.BackgroundColor = ConsoleColor.Red;
+            else
+                Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
